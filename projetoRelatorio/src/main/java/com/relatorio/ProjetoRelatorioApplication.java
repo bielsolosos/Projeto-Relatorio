b476387b4381b57f7f395f9ade90cf490a454e85
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2024-08-28 13:09:56 -0300

    Inicializando o Projeto

---
+++ projetoRelatorio/src/main/java/com/relatorio/ProjetoRelatorioApplication.java
@@ -0,0 +1,13 @@
+package com.relatorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRelatorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRelatorioApplication.class, args);
+	}
+
+}
---
+++ projetoRelatorio/src/test/java/com/relatorio/ProjetoRelatorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRelatorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
